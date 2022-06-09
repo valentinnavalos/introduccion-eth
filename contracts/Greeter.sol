423afdaa0c2ecb0e894e4c12b3cdefa07240a04d
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2022-06-09 17:17:21 -0300

    feat: add comments of smart contracts
    Add comments about some features of a smart contract.

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,22 +1,28 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+// solidity version that was used to generate this file
+pragma solidity ^0.8.0; //--> "cualquier versión mayor o igual a 0.8.0 messirve"
+//              >SemVer --> Semantic Versioning
 
-import "hardhat/console.sol";
-
+// cada 'contract' es un contrato, hence podemos tener 70 en un mismo archivo. y van a hacer todos distintos.
 contract Greeter {
-    string private greeting;
+    // atributos del contrato. Sus variables.
+    string private greeting; //--> estado de memoria persistente.
 
+    // código que se ejecuta al momento de hacer el deployment.--> subir el contrato a la blockchain.
+    // luego del success deploy, se dropea el constructor, se borra.
     constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
+        greeting = _greeting;//--> defino el valor de greeting.
     }
 
+    // --- función de lectura. ---
+    // que sea 'public' significa que CUALQUIERA PUEDE LLAMAR A ESTA FUNCIÓN.
+    // que sea 'view' significa que no se van a realizar cambios en el storage.
     function greet() public view returns (string memory) {
         return greeting;
     }
 
+    // --- función de escritura. --- 
     function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
         greeting = _greeting;
     }
 }

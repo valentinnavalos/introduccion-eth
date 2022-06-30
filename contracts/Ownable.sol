0a775aa81b196626c3b1d45a3c792ad133aab422
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2022-06-30 00:27:06 -0300

    fix: rename variables

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -3,12 +3,17 @@ pragma solidity ^0.8.0;
 
 contract Ownable {
     // address es un tipo de dato nativo y simple (no complejo) de solidity.
-    address public owner;
+    address public _owner;
     // public --> cualquier persona puede venir y preguntar quien es el dueño.
+
+    // tiene 2 parámetros: previousOwner y newOwner.
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    // se indexa para que sea más eficiente.
+
 
     // modifier: es una función que se ejecuta antes de que se ejecute la función que se le pasa como parámetro.
     modifier onlyOwner {
-        require(msg.sender == owner);
+        require(msg.sender == _owner);
         // acá estamos seteando que se llame a transferOwnership() solo si msg.sender es 'owner'.
         // "requiero que el msg.sender sea owner."
 
@@ -18,13 +23,14 @@ contract Ownable {
 
         _;
         // _ --> es una variable que se usa para que se ejecute la función que se le pasa como parámetro.
+        // que corra el cuerpo de la fn modificada.
     }
     // --> es una fn modificadora.
     // los modifiers pueden ser de visiblidad, pueden ser custom.
 
-    constructor(address _owner) {
+    constructor(address owner) {
         //--> los constructores no requieren visibilidad.
-        owner = _owner;
+        _owner = _owner;
         // al deployar el contrato, le voy a pasar al constructor la address que quiero que sea el dueño.
     }
 
@@ -35,7 +41,11 @@ contract Ownable {
         // esta fn es pública porque queremos que el dueño llame a esta función.
         // si fuese privada, solo se podria llamar desde dentro del contrato. 
         // --> lo mismo que dejar la llave adentro. 
-        owner = newOwner;
+        address previousOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(previousOwner, newOwner);
+        // si quiero saber todos los dueños que tuvo este contrato, voy a mirar los logs de este contrato,
+        // y mirar todos los eventos OwnershipTransferred que hubo.
     }
     // suele heredarse de este contrato, para escribirse otros contratos que tengan un dueño tmb.
     // y utilizar esta misma fn.

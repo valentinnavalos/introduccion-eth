b234fd40b88d510f301913a12549a4458525bf3a
Valentín Avalos <89805271+valentinnavalos@users.noreply.github.com>
2022-07-01 12:44:05 -0300

    feat: smart contract deployed sucessfully

--- scripts/ownable-deployment.js
+++ scripts/ownable-deployment.js
@@ -12,12 +12,12 @@ async function main() {
   // If this script is run directly using `node` you may want to call compile
   // manually to make sure everything is compiled
   // await hre.run('compile');
-  const [signer] = await hre.ethers.getSigners();
+  const [martin, juana] = await hre.ethers.getSigners();
   // signer es una cta de eth para deployar.
 
   // We get the contract to deploy
   const Ownable = await hre.ethers.getContractFactory("Ownable");
-  const ownable = await Ownable.deploy(signer.address);
+  const ownable = await Ownable.deploy(martin.address);
   // dentro de .deploy() van los parametros que requiere el constructor.
 
   await ownable.deployed();
@@ -26,6 +26,8 @@ async function main() {
   console.log("Ownable deployed to:", ownable.address);
   const foo = await ownable.owner();
   console.log("Este es el owner: ", foo);
+
+  await ownable.transferOwnership(juana.address)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
